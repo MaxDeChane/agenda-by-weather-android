3646165492f7508f67fe5728e91ac6d6216b34a9
MaxDeChane <d3chan3m@gmail.com>
2025-10-21 20:44:53 -0500

    Add check so weather isn't updated unnecessarily when LocationView is first opened.

--- app/src/main/java/com/example/weatherbyagendaandroid/presentation/composable/menu/LocationView.kt
+++ app/src/main/java/com/example/weatherbyagendaandroid/presentation/composable/menu/LocationView.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,14 +29,25 @@ fun LocationView(locationViewModel: LocationViewModel = viewModel(),
                  weatherViewModel: WeatherViewModel = viewModel()
 ) {
     var showAddLocationAlertView by remember { mutableStateOf(false) }
+    val isFirstTimeThrough = remember { mutableListOf(true) }
 
     val gpsLocation by locationViewModel.gpsLocation.collectAsStateWithLifecycle()
     val currentSelectedLocation by locationViewModel.selectedSavedLocation.collectAsStateWithLifecycle()
 
-    if(currentSelectedLocation != null) {
-        weatherViewModel.updateWeatherInfo(currentSelectedLocation!!.latitude, currentSelectedLocation!!.longitude)
-    } else if(gpsLocation != null) {
-        weatherViewModel.updateWeatherInfo(gpsLocation!!.latitude, gpsLocation!!.longitude)
+    LaunchedEffect(currentSelectedLocation) {
+        // Don't reload the weather if it is the first time this is loaded since
+        // it couldn't have changed if the options haven't been displayed yet.
+        if(!isFirstTimeThrough[0]) {
+            if (currentSelectedLocation != null) {
+                weatherViewModel.updateWeatherInfo(
+                    currentSelectedLocation!!.latitude,
+                    currentSelectedLocation!!.longitude
+                )
+            } else if (gpsLocation != null) {
+                weatherViewModel.updateWeatherInfo(gpsLocation!!.latitude, gpsLocation!!.longitude)
+            }
+        }
+        isFirstTimeThrough[0] = false
     }
 
     val currentSavedLocationId = if(currentSelectedLocation != null) currentSelectedLocation!!.id else -1
