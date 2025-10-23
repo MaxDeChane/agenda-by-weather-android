9b02d9ef5124a478de54ef4f6d6a7d5867110f69
MaxDeChane <d3chan3m@gmail.com>
2025-10-23 17:16:52 -0500

    Clean up imports

--- app/src/main/java/com/example/weatherbyagendaandroid/MainActivity.kt
+++ app/src/main/java/com/example/weatherbyagendaandroid/MainActivity.kt
@@ -2,49 +2,34 @@ package com.example.weatherbyagendaandroid
 
 import WeatherByAgendaAndroidTheme
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.weatherbyagendaandroid.enums.LoadingStatusEnum
 import com.example.weatherbyagendaandroid.helpers.LocationHelper
-import com.example.weatherbyagendaandroid.presentation.composable.UpdateSettingsPermissionRequestView
-import com.example.weatherbyagendaandroid.presentation.composable.UpdateUserPermissionRequestView
-import com.example.weatherbyagendaandroid.presentation.composable.WeatherPeriodsView
-import com.example.weatherbyagendaandroid.presentation.composable.WeatherTopBar
-import com.example.weatherbyagendaandroid.presentation.composable.menu.MenuView
 import com.example.weatherbyagendaandroid.notification.createFromChannelId
 import com.example.weatherbyagendaandroid.presentation.composable.AgendaItemWeatherPeriodsView
 import com.example.weatherbyagendaandroid.presentation.composable.GeneralWeatherDisplay
-import com.example.weatherbyagendaandroid.presentation.model.AgendaViewModel
-import com.example.weatherbyagendaandroid.presentation.model.LocationViewModel
+import com.example.weatherbyagendaandroid.presentation.composable.UpdateSettingsPermissionRequestView
+import com.example.weatherbyagendaandroid.presentation.composable.UpdateUserPermissionRequestView
+import com.example.weatherbyagendaandroid.presentation.composable.WeatherTopBar
+import com.example.weatherbyagendaandroid.presentation.composable.menu.MenuView
 import com.example.weatherbyagendaandroid.presentation.model.MenuViewModel
 import com.example.weatherbyagendaandroid.presentation.model.PermissionsViewModel
-import com.example.weatherbyagendaandroid.presentation.model.WeatherFilterViewModel
-import com.example.weatherbyagendaandroid.presentation.model.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
